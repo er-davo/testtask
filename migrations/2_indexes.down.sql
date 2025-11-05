0ff3580616f48e12ecf1f6cbc656808a2d6a8119
Eritsian David <wruser123@gmail.com>
2025-11-05 19:33:52 +0300

    indexes added

---
+++ migrations/2_indexes.down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_subscriptions_user_period;
+DROP INDEX IF EXISTS idx_subscriptions_service_name;
+DROP INDEX IF EXISTS idx_subscriptions_user_id;
---
+++ migrations/2_indexes.up.sql
@@ -0,0 +1,8 @@
+CREATE INDEX IF NOT EXISTS idx_subscriptions_user_id
+ON subscriptions(user_id);
+
+CREATE INDEX IF NOT EXISTS idx_subscriptions_service_name
+ON subscriptions(service_name);
+
+CREATE INDEX IF NOT EXISTS idx_subscriptions_user_period
+ON subscriptions(user_id, start_date, end_date);
